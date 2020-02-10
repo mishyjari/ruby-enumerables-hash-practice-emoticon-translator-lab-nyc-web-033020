9d113a8b7e19acddd4442ea4ff0d4a0efedf837d
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 17:20:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(path)
   lib = YAML.load_file(path)
   #pp lib
   keys = {
-    get_meaning: lib.reduce({}) do | memo, (key,value) | memo[key] = value; pp memo end,
+    get_meaning: lib.reduce({}) do | memo, (key,value) | memo[key] = value[1]; pp memo end,
  #   get_emoticon: lib.reduce({}) do | memo, (key,value) | memo[key] => value[0] end
   }
   keys
